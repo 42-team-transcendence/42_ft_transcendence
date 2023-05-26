0cef53cefd04c425c38241f03abfe2f60e8afd8b
Camille CALAS <ccalas@e3r3p11.clusters.42paris.fr>
2023-05-26 11:43:08 +0200

    try and catch

--- project-test/src/auth/auth.service.ts
+++ project-test/src/auth/auth.service.ts
@@ -1,26 +1,40 @@
-import { Injectable } from "@nestjs/common";
+import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { AuthoDto } from "./dto";
-import * as argon from 'argon2'
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService) {}
     //TODO check away + async
     async signup(dto: AuthoDto) {
+
         // generate the password hash
         const hash = await argon.hash(dto.password);
-        // save the new user in the db
-        const user = await this.prisma.user.create({
-            data: {
-                nickName: dto.nickname,
-                email: dto.email,
-                hash,
-            },
-        });
 
-        delete user.hash;
-        return user;
+        // save the new user in the db
+        try {
+            const user = await this.prisma.user.create({
+                data: {
+                    nickName: dto.nickName,
+                    email: dto.email,
+                    hash,
+                },
+            
+            });
+            delete user.hash;
+            //return the save user
+            return user;
+            
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
+                    throw new ForbiddenException('Credentials taken',);
+                }
+            }
+            throw error;
+        }
     }
 
     signin() {
--- project-test/src/auth/dto/auth.dto.ts
+++ project-test/src/auth/dto/auth.dto.ts
@@ -3,7 +3,7 @@ import { IsEmail, IsNotEmpty, IsString } from "class-validator";
 export class AuthoDto {
     @IsString()
     @IsNotEmpty()
-    nickname:   string;
+    nickName:   string;
 
     @IsEmail()
     @IsNotEmpty()
