b565e47c94cfd9bddafe0b8fa2768667e5d1bcb6
Vladimir Nafissi <vlad.nafissi@gmail.com>
2023-06-04 09:31:27 +0200

    rename folder server

--- project-test/src/prisma/prisma.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Global() //décorateur permettant d'utiliser ce module dans tous nos autres modules
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
-})
-export class PrismaModule {}
