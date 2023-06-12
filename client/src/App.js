ce6112da5211375fafe1aa0326120f055cd32964
Vladimir Nafissi <vlad.nafissi@gmail.com>
2023-06-12 17:26:39 +0200

    first steps, ça marche pas bien

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import Register from './Register';
+import React from "react";
+
+
+// function App() {
+
+//   return (
+//     <main className="App">
+//       <Register />
+//       <h1>TEST</h1> 
+//     </main>
+//   );
+// }
+
+const App = () =>{
+    return (
+        <main className="App">
+        {/* <Register /> */}
+          <h1>TEST</h1>
+        </main>
+    )
+}
+
+export default App
