1a8b4055a80ee7c4bbd4ad52576650306e2b58ba
Vladimir Nafissi <vlad.nafissi@gmail.com>
2023-06-14 14:44:53 +0200

    formulaire renvoyant un succes

--- client/src/Register.js
+++ client/src/Register.js
@@ -46,103 +46,127 @@ export default function Register() {
         setErrMsg('');
     }, [user, pwd, matchPwd])
 
-    return (
-    <section>
-        <p ref={errRef} className={errMsg? "errmsg" : "offscreen"}>
-        </p>
-        <h1>Register</h1>
-        <form >
-            <label htmlFor="username">
-                Username:
-                <span className={validName ? "valid" : "hide"}>
-                    <FontAwesomeIcon icon={faCheck} />
-                </span>
-                <span className={validName || !user ? "hide" : "invalid"}>
-                    <FontAwesomeIcon icon={faTimes} />
-                </span>
-            </label>
-            <input
-                type="text"
-                id="username" //même valeur que le label
-                ref={userRef}
-                autoComplete="off"
-                onChange={(e) => setUser(e.target.value)}
-                value={user}
-                required // champ requis
-                onFocus={() => setUserFocus(true)}
-                onBlur={() => setUserFocus(false)}
-            />
-            <p className={userFocus && user && !validName ? "instructions" : "offscreen"} >
-                <FontAwesomeIcon icon={faInfoCircle} />
-                4 to 24 characters.<br/>
-                Must begin with a letter.<br/>
-                Letters, numbers, underscores, hypens allowed
-            </p>
 
-            <label htmlFor="password">
-                Password:
-                <span className={validPwd ? "valid" : "hide"}>
-                    <FontAwesomeIcon icon={faCheck} />
-                </span>
-                <span className={validPwd || !pwd ? "hide" : "invalid"}>
-                    <FontAwesomeIcon icon={faTimes} />
-                </span>
-            </label>
-            <input
-                type="password"
-                id="password" //même valeur que le label
-                ref={userRef}
-                onChange={(e) => setPwd(e.target.value)}
-                required // champ requis
-                onFocus={() => setPwdFocus(true)}
-                onBlur={() => setPwdFocus(false)}
-            />
-            <p className={pwdFocus && user && !validPwd ? "instructions" : "offscreen"} >
-                <FontAwesomeIcon icon={faInfoCircle} />
-                8 to 24 characters.<br/>
-                Must include uppercase and lowercase character, a number and a special character.<br/>
-                Allowed special characters: !@#$%
-            </p>
-
-            <label htmlFor="confirm_pwd">
-                Confirm Password:
-                <span className={validMatch && matchPwd ? "valid" : "hide"}>
-                    <FontAwesomeIcon icon={faCheck} />
-                </span>
-                <span className={validMatch || !matchPwd ? "hide" : "invalid"}>
-                    <FontAwesomeIcon icon={faTimes} />
-                </span>
-            </label>
-            <input
-                type="password"
-                id="confirm_pwd" //même valeur que le label
-                ref={userRef}
-                onChange={(e) => setMatchPwd(e.target.value)}
-                required // champ requis
-                onFocus={() => setMatchFocus(true)}
-                onBlur={() => setMatchFocus(false)}
-            />
-            <p className={matchFocus && user && !validMatch ? "instructions" : "offscreen"} >
-                <FontAwesomeIcon icon={faInfoCircle} />
-                Must match the first password input field.
-            </p>
-
-            <button disabled={!validName || !validPwd || !validMatch ? true : false }>
-                Sign Up
-            </button>
-
-            <p>
-                Already Registered ?<br />
-                <span className="line">
-                    {/* TODO Put router link here */}
-                    {/* // placeholder link */}
-                    <a href="#">Sign In</a>
-                </span>
-            </p>
-        </form>
-    </section>
-
+    const handleSubmit = async (e) => {
+        e.preventDefault(); //on enlève le comportement par défaut du bouton pour mettre le n$otre
         
-    )
+        //if button enabled with JS hack
+        const v1 = USER_REGEX.test(user);
+        const v2 = PWD_REGEX.test(pwd);
+        if (!v1 || !v2) {
+            setErrMsg("Invalid Entry");
+            return;
+        }
+        console.log(user, pwd);
+        setSuccess(true);
+    }
 
+
+    return (
+        <>
+        {success ? (
+            <section>
+                <h1>Success !</h1>
+                <p>
+                    <a href="#">Sign In</a>
+                </p>
+            </section>
+        ) : (
+        <section>
+            <p ref={errRef} className={errMsg? "errmsg" : "offscreen"}>
+            </p>
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="username">
+                    Username:
+                    <span className={validName ? "valid" : "hide"}>
+                        <FontAwesomeIcon icon={faCheck} />
+                    </span>
+                    <span className={validName || !user ? "hide" : "invalid"}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </span>
+                </label>
+                <input
+                    type="text"
+                    id="username" //même valeur que le label
+                    ref={userRef}
+                    autoComplete="off"
+                    onChange={(e) => setUser(e.target.value)}
+                    value={user}
+                    required // champ requis
+                    onFocus={() => setUserFocus(true)}
+                    onBlur={() => setUserFocus(false)}
+                />
+                <p className={userFocus && user && !validName ? "instructions" : "offscreen"} >
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    4 to 24 characters.<br/>
+                    Must begin with a letter.<br/>
+                    Letters, numbers, underscores, hypens allowed
+                </p>
+
+                <label htmlFor="password">
+                    Password:
+                    <span className={validPwd ? "valid" : "hide"}>
+                        <FontAwesomeIcon icon={faCheck} />
+                    </span>
+                    <span className={validPwd || !pwd ? "hide" : "invalid"}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </span>
+                </label>
+                <input
+                    type="password"
+                    id="password" //même valeur que le label
+                    ref={userRef}
+                    onChange={(e) => setPwd(e.target.value)}
+                    required // champ requis
+                    onFocus={() => setPwdFocus(true)}
+                    onBlur={() => setPwdFocus(false)}
+                />
+                <p className={pwdFocus && user && !validPwd ? "instructions" : "offscreen"} >
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    8 to 24 characters.<br/>
+                    Must include uppercase and lowercase character, a number and a special character.<br/>
+                    Allowed special characters: !@#$%
+                </p>
+
+                <label htmlFor="confirm_pwd">
+                    Confirm Password:
+                    <span className={validMatch && matchPwd ? "valid" : "hide"}>
+                        <FontAwesomeIcon icon={faCheck} />
+                    </span>
+                    <span className={validMatch || !matchPwd ? "hide" : "invalid"}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </span>
+                </label>
+                <input
+                    type="password"
+                    id="confirm_pwd" //même valeur que le label
+                    ref={userRef}
+                    onChange={(e) => setMatchPwd(e.target.value)}
+                    required // champ requis
+                    onFocus={() => setMatchFocus(true)}
+                    onBlur={() => setMatchFocus(false)}
+                />
+                <p className={matchFocus && user && !validMatch ? "instructions" : "offscreen"} >
+                    <FontAwesomeIcon icon={faInfoCircle} />
+                    Must match the first password input field.
+                </p>
+
+                <button disabled={!validName || !validPwd || !validMatch ? true : false }>
+                    Sign Up
+                </button>
+
+                <p>
+                    Already Registered ?<br />
+                    <span className="line">
+                        {/* TODO Put router link here */}
+                        {/* // placeholder link */}
+                        <a href="#">Sign In</a>
+                    </span>
+                </p>
+            </form>
+        </section>
+        )}
+        </>
+    )
 }
