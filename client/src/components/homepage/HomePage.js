aa3c30ada3ac2820e33e2f443bdf871b5fc9ef23
camille <camillecalas@hotmail.fr>
2023-06-19 16:54:47 +0200

    jwt refresh token video tuto

--- client/src/components/homepage/HomePage.js
+++ client/src/components/homepage/HomePage.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
+import Leaderboard from "../leaderboard/Leaderboard";
 
 function Homepage() {
+
+    const navigate = useNavigate();
+    const from_leaderboard = "/leaderboard"; 
+
+    const handleLeaderboard = () => {
+        navigate(from_leaderboard, { replace: false});
+    }
+
 	return (
+		<div>
 		<h1> WELCOME TO PONG </h1>
+
+		<label><u>Leaderboard </u> </label>
+		<button onClick={handleLeaderboard}>Leaderboard</button>
+		</div>
 	)
 }
 
---
+++ client/src/components/leaderboard/Leaderboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Users from '../users/Users'
+
+function Leaderboard() {
+  return (
+	<div>Leaderboard
+		< Users />
+	</div>
+  )
+}
+
+export default Leaderboard
