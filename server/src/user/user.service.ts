adc03fc6834cf3ae93ef881e456a2a5677af02d4
Vladimir Nafissi <vlad.nafissi@gmail.com>
2023-06-20 12:41:48 +0200

    backend:  create a "/users" endpoint

---
+++ server/src/user/user.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class UserService {
+    constructor(
+        private prisma: PrismaService,
+    ) {}
+    
+    async findAllUsers() {
+        const users = await this.prisma.user.findMany();
+        return users;
+    }
+}
