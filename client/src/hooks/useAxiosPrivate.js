d2cc112ab41509497d0db98c573ea62c0375a3fd
camille <camillecalas@hotmail.fr>
2023-06-21 12:12:16 +0200

    add commentaires sur axios private

--- client/src/hooks/useAxiosPrivate.js
+++ client/src/hooks/useAxiosPrivate.js
@@ -61,27 +61,3 @@ export default useAxiosPrivate
 
 
 
-
-/*
-The axios.interceptors.request.use() and axios.interceptors.response.use() methods are both used to intercept HTTP requests and responses, but they are applied at different stages of the request lifecycle and serve different purposes:
-
-axios.interceptors.request.use(): This method is used to intercept and modify the request configuration before the request is sent to the server. It allows you to add headers, modify the URL, attach authentication tokens, or perform any other transformations on the request. It is typically used to add global headers or authentication tokens that need to be included in every outgoing request. The request interceptor is executed before the request is sent.
-
-axios.interceptors.response.use(): This method is used to intercept and process the response returned by the server before it is passed to the application code. It allows you to handle common response transformations or error handling logic in a centralized manner. You can modify the response data, check for errors, perform logging, or implement any custom logic based on the response. The response interceptor is executed after the response is received from the server but before it is passed to the .then() or .catch() handlers. 
-*/
-
-
-/* 
-What are axios interceptors
-
-Axios interceptors are functions that allow you to intercept and modify HTTP requests or responses globally before they are handled by your application. They provide a way to add custom logic or transformations to requests or responses across your entire Axios instance.
-
-Axios interceptors can be useful in various scenarios, such as:
-
-Authentication: You can use an interceptor to automatically add authentication headers to outgoing requests based on the user's authentication status or access token.
-
-Logging: Interceptors can log incoming requests and outgoing responses for debugging purposes or to collect analytics data.
-
-Error handling: You can use interceptors to globally handle and process specific error responses, such as handling unauthorized requests or redirecting to a login page.
-
-*/
