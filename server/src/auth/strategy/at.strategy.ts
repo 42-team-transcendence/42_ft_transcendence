696cd31bac42212782b9a79e1989d675ebab8be2
Vladimir Nafissi <vlad.nafissi@gmail.com>
2023-06-22 20:35:03 +0200

    adapted strategies to double tokens, added comments, adapted /refresh route within auth controller

--- server/src/auth/strategy/at.strategy.ts
+++ server/src/auth/strategy/at.strategy.ts
@@ -9,27 +9,35 @@ type JwtPayload = {
     email:string
 }
 
+//Explication des stratégies passeport ici https://docs.nestjs.com/recipes/passport
 @Injectable()
 export class AtJwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     constructor (config : ConfigService, private prisma : PrismaService) {
         super ({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), //recuperation auto du token dans les headers lors d'une requete
-            secretOrKey: config.get('JWT_SECRET'),
+            secretOrKey: config.get<string>('JWT_SECRET'),
         })
     };
 
+
+    //la fonction validate est standard et définie par la classe PassportStrategy
     //sert à extraire les données du jwt et à les comparer à la DB pour valider le user
     //tout ce qui est retourné avec la fonction validate est passée aux fonctions suivantes
     // via l'objet request d'Express, dans l'objet "user"
-    async validate(payload : JwtPayload) {
-        console.log({payload});
+    async validate(payload : JwtPayload): Promise<any> {
+        console.log({"jwt_payload_validate" : payload});
         const user = await this.prisma.user.findUnique({
             where: {
                 id: payload.sub,
             },
         });
-        console.log({user});
+        console.log({"jwt_user_validate" : user});
         delete user.hash;
         return user;
     };
+
+    // validate(payload: JwtPayload) {
+    //     return payload;
+    //   }
+
 }
