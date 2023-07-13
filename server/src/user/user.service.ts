67f917edc1204698ad5d495a8f52cf73b23503fe
Vladimir Nafissi <vlad.nafissi@gmail.com>
2023-07-13 17:22:06 +0200

    update nestjs and prisma packages with new package npm-check-updates. install websocket packages

--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,7 +1,6 @@
 import { ForbiddenException, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import * as argon from 'argon2';
-import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import { JwtService } from "@nestjs/jwt";
 import { ConfigService } from "@nestjs/config";
 
