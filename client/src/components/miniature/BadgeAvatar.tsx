dc27f760354ac83dbc4fa70666d860bfa5defdd6
Vladimir Nafissi <vlad.nafissi@gmail.com>
2023-07-14 22:36:17 +0200

    create a customized avatar and miniature

--- client/src/components/miniature/BadgeAvatar.tsx
+++ client/src/components/miniature/BadgeAvatar.tsx
@@ -2,53 +2,33 @@ import { styled } from '@mui/material/styles';
 import Badge from '@mui/material/Badge';
 import Avatar from '@mui/material/Avatar';
 import tchoupi from '../../assets/tchoupi50x50.jpg'
-
-
-// const StyledBadge = styled(Badge)(({ theme }) => ({
-//   '& .MuiBadge-badge': {
-//     backgroundColor: '#44b700',
-//     color: '#44b700',
-//     boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
-//     '&::after': {
-//       position: 'absolute',
-//       top: 0,
-//       left: 0,
-//       width: '100%',
-//       height: '100%',
-//       borderRadius: '50%',
-//       animation: 'ripple 1.2s infinite ease-in-out',
-//       border: '1px solid currentColor',
-//       content: '""',
-//     },
-//   },
-//   '@keyframes ripple': {
-//     '0%': {
-//       transform: 'scale(.8)',
-//       opacity: 1,
-//     },
-//     '100%': {
-//       transform: 'scale(2.4)',
-//       opacity: 0,
-//     },
-//   },
-// }));
-
-// const SmallAvatar = styled(Avatar)(({ theme }) => ({
-//   width: 22,
-//   height: 22,
-//   border: `2px solid ${theme.palette.background.paper}`,
-// }));
+import { useState } from 'react';
 
 export default function BadgeAvatar() {
+  const [isConnected, updateIsConnected] = useState<boolean>(true)
+  const [invisible, setInvisible] = useState(false); //faire disparaître le rond de connexion
+
+  // The "styled" function from @mui/material/styles is a utility provided by MUI
+  // that allows you to create custom styled components easily. 
+  // It is based on the concept of CSS-in-JS, where you define styles directly in your JS code.
+  // The styled function takes two arguments: a component and a style definition function (propriétés CSS)
+  const StyledBadge = styled(Badge)(() => (
+  {
+    '& .MuiBadge-badge': {
+      backgroundColor: isConnected ? 'green' : 'grey',
+      color: isConnected ? 'green' : 'grey',
+      boxShadow: "0 0 0 2px white",
+    },
+  }));
+
   return (
-    //   <StyledBadge
-    //     overlap="circular"
-    //     anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-    //     variant="dot"
-    //   >
+      <StyledBadge
+        overlap="circular"
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        variant="dot"
+        invisible={invisible}
+      >
         <Avatar alt="Tchoupi" src={tchoupi} />
-    //   </StyledBadge>
+      </StyledBadge>
   );
 }
-
-// src="../../assets/tchoupi50x50.jpg"
