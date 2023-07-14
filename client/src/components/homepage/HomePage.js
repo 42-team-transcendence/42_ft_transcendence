a1a7aa0fc7d6972f89003f670f6f205d48b75460
camille <camillecalas@hotmail.fr>
2023-07-14 17:03:33 +0200

    front

--- client/src/components/homepage/HomePage.js
+++ client/src/components/homepage/HomePage.js
@@ -1,55 +1,54 @@
 import { useNavigate } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import Navbar from "../navbar/Navbar";
-//import HomepageStyle from '../../styles/HomepageStyle.css';
+
+
+import HomepageStyle from '../../styles/HomepageStyle.css';
 import CustomButton from "../../styles/buttons/CustomButton";
 
 
 function Homepage() {
 
     const navigate = useNavigate();
-    const from_leaderboard = "/leaderboard";
+    const from_leaderboard = "/leaderboard"; 
 	const from_how_to_play = "/rules";
 	const from_play = "/play";
 	const profile = "/profile";
 	const friend_list = "/friendlist";
 
+
 	const handlePlay = () => {
 		navigate(from_play, {replace: false});
 	}
-	
+
     const handleLeaderboard = () => {
         navigate(from_leaderboard, { replace: false});
     }
-
-
-	const [showLinks, setShowLinks] = useState(false)
-
-	const handleShowLinks = () => {
-		setShowLinks(!showLinks)
-	}
-
 
 	const handleRules = () => {
 		navigate(from_how_to_play, {replace: false});
 	}
 
-	
+	const [showLinks, setShowLinks] = useState(false)
+	const handleShowLinks = () => {
+		setShowLinks(!showLinks)
+	}
+
 	return (
 		<div className="column">
 			<Navbar showLinks={showLinks} handleShowLinks={handleShowLinks}/>
-			<div className="welcomeHome">
-				<h1 className="welcome"> Welcome </h1>
-				<h1 className="welcome"> to </h1>
-				<h1 className="titleHomePage"> PONG </h1>
-				<></>
+			<div className="backgroundTop">
+				<div className="welcomeHome">
+					<h1 className="welcome"> Welcome </h1>
+					<h1 className="welcome"> to </h1>
+					<h1 className="titleHomePage"> PONG </h1>
+				</div>
 			</div>
-				<div className="ButtonHomePage">
-        <div className={`${showLinks ? "hide_button" : "show_button"}`}>
+		
+			<div className={`${showLinks ? "hide_button" : "show_button"}`}>
 					<CustomButton onClick={handlePlay}> PLAY </CustomButton>
 					<CustomButton onClick={handleRules}> How to Play </CustomButton>
 					<CustomButton onClick={handleLeaderboard}> Leaderboard </CustomButton>
-          </div>
 			</div>
 		</div>
 	)
--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,101 +1,22 @@
-// import React, { useState, useEffect } from 'react';
-// import SearchAppBar from './SearchBar';
-// import CustomButton from "../../styles/buttons/CustomButton";
-// import Box from '@mui/material/Box';
-// import Logout from '../logout/Logout';
-// import '../../styles/Navbar.css';
-// import { IconButton } from '@mui/material';
-// import MenuIcon from '@mui/icons-material/Menu';
-// import CloseIcon from '@mui/icons-material/Close';
-// import { Routes, Route } from 'react-router-dom';
-// import { Link } from 'react-router-dom'; // Add this import statement
-
-
-// function Navbar() {
-//   const [isBurgerOpen, setIsBurgerOpen] = useState(false);
-//   const [isMobile, setIsMobile] = useState(window.innerWidth <= 1050);
-
-//   const toggleBurgerMenu = () => {
-//     setIsBurgerOpen(!isBurgerOpen);
-//   };
-
-//   useEffect(() => {
-//     const handleResize = () => {
-//       setIsMobile(window.innerWidth <= 1050);
-//     };
-
-//     window.addEventListener('resize', handleResize);
-
-//     return () => {
-//       window.removeEventListener('resize', handleResize);
-//     };
-//   }, []);
-
-//   return (
-//     <header className={`header ${isBurgerOpen ? 'menu-open' : ''}`}>
-//       <h2>PONG</h2>
-//       {(isMobile && !isBurgerOpen) ? (
-//         <IconButton
-//           className={`burger ${isBurgerOpen ? 'open' : ''}`}
-//           color="inherit"
-//           onClick={toggleBurgerMenu}
-//         >
-//           {isBurgerOpen ? <CloseIcon /> : <MenuIcon />}
-//         </IconButton>
-//       ) : (
-//         <nav className={`menu ${isBurgerOpen ? 'burger-menu' : ''}`}>
-//           <a className='textMenu'>Play</a>
-//           <a className='textMenu'>Chat & Channels</a>
-//           <a className='textMenu'>Friends List</a>
-//           <a className='textMenu'>View/Change profile</a>
-//           <SearchAppBar />
-//           <Logout />
-//         </nav>
-//       )}
-//     </header>
-//   );
-// }
-
-// export default Navbar;
-
-import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import SearchAppBar from './SearchBar';
-import CustomButton from "../../styles/buttons/CustomButton";
-import Box from '@mui/material/Box';
 import Logout from '../logout/Logout';
 import '../../styles/Navbar.css';
-import { IconButton } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
-import Profile from '../profile/Profile';
 
 function Navbar({showLinks, handleShowLinks}) {
-	return (
 
+	return (
 		<nav className={`navbar  ${showLinks ? "show_nav" : "hide_nav"}`}>
 			<div className='navbar_logo'> PONG </div>
-			<ul className='navbar_links'>
-				<li className='navbar_items'>
-				<a href='/' className='navbar_link'>Play</a>
-				</li>
-				<li className='navbar_items'>
-				<a href='/' className='navbar_link'>Chat & Channels</a>
-				</li>
-				<li className='navbar_items'>
-				<a href='/' className='navbar_link'>Friends List</a>
-				</li>
-				<li className='navbar_items'>
-				<a href='/' className='navbar_link'>View/Change profile</a>
-				</li>
-				<li className='navbar_items'>
-					<SearchAppBar />
-				</li>
-				<li className='navbar_items'>
-					<Logout className='logout_item'/>
-				</li>
-    	 		
-			</ul>
+			<div className='navbar_links'>
+				<Link to="/" className="navbar_link">Home</Link>
+				<Link to="/play" className="navbar_link">Play</Link>
+				<Link to="/chat" className="navbar_link">Chat & Channels</Link>
+				<Link to="/friendlist" className="navbar_link">Friends List</Link>
+				<Link to="/profile" className="navbar_link">View/Change Profile</Link>
+				<SearchAppBar />
+				<Logout className='logout_item'/>
+			</div>
 			<button className='navbar_burger' onClick={handleShowLinks}>
 				<span className='burger_bar'></span>
 			</button>
@@ -105,51 +26,3 @@ function Navbar({showLinks, handleShowLinks}) {
 }
 
 export default Navbar;
-
-
-
-
-// function Navbar() {
-//   const [isBurgerOpen, setIsBurgerOpen] = useState(false);
-//   const [isMobile, setIsMobile] = useState(window.innerWidth <= 1050);
-
-//   const toggleBurgerMenu = () => {
-//     setIsBurgerOpen(!isBurgerOpen);
-//   };
-
-//   useEffect(() => {
-//     const handleResize = () => {
-//       setIsMobile(window.innerWidth <= 1050);
-//     };
-
-//     window.addEventListener('resize', handleResize);
-
-//     return () => {
-//       window.removeEventListener('resize', handleResize);
-//     };
-//   }, []);
-
-//   return (
-//     <header className={`header ${isBurgerOpen ? 'menu-open' : ''}`}>
-//       <h2>PONG</h2>
-//       {(isMobile && !isBurgerOpen) ? (
-//         <IconButton
-//           className={`burger ${isBurgerOpen ? 'open' : ''}`}
-//           color="inherit"
-//           onClick={toggleBurgerMenu}
-//         >
-//           {isBurgerOpen ? <CloseIcon /> : <MenuIcon />}
-//         </IconButton>
-//       ) : (
-//         <nav className={`menu ${isBurgerOpen ? 'burger-menu' : ''}`}>
-//           <a className='textMenu'>Play</a>
-//           <a className='textMenu'>Chat & Channels</a>
-//           <a className='textMenu'>Friends List</a>
-//           <a className='textMenu'>View/Change profile</a>
-//           <SearchAppBar />
-//           <Logout />
-//         </nav>
-//       )}
-//     </header>
-//   );
-// }
