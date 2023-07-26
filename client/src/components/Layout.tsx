baa85c31e24808fdfb7d778c09a6439d4143192b
Maryna SAMOILENKO <masamoil@bess-f4r6s1.clusters.42paris.fr>
2023-07-26 11:50:59 +0200

    layout, logout, hooks => typescript

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 
-const Layout = () => {
+const Layout: React.FC = () => {
     return (
         <main className="App">
             <Outlet />
--- client/src/components/logout/Logout.tsx
+++ client/src/components/logout/Logout.tsx
@@ -1,11 +1,10 @@
 import { Link, useNavigate } from "react-router-dom"
-
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 import useAuth from "../../hooks/useAuth";
 
 import '../../styles/Navbar.css';
 
-export default function Logout () {
+const Logout: React.FC = () => {
 
     const from_signup = "/register"; 
     const axiosPrivate = useAxiosPrivate();
@@ -16,11 +15,13 @@ export default function Logout () {
     const handleClick = () => {
 
         axiosPrivate.post('/auth/logout');
-        setAuth({});// supprime l'accessToken du state auth.
+        setAuth({ accessToken: '', refreshToken: '' });// supprime l'accessToken du state auth.
         navigate(from_signup, { replace: true});//redirige vers la page register en remplacant l'historique.
     }
 
     return (
-		<Link onClick={handleClick} className="textLogout">Log out</Link>
+		<Link to="/register" onClick={handleClick} className="textLogout">Log out</Link>
     )
 }
+
+export default Logout;
