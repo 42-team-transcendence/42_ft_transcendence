a73a1d346e9a72ababa23826b3f2bf60d355ca0d
camille <camillecalas@hotmail.fr>
2023-08-07 15:08:43 +0200

    cleaning

--- client/src/components/profile/PasswordModal.tsx
+++ client/src/components/profile/PasswordModal.tsx
@@ -9,69 +9,69 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 
 interface PwdModalProps {
-  open: boolean;
-  onClose: () => void;
-  onSave: (newPwd: string) => void;
+	open: boolean;
+	onClose: () => void;
+	onSave: (newPwd: string) => void;
 }
 
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 const PwdModal: React.FC<PwdModalProps> = ({ open, onClose, onSave }) => {
-  const [newPwd, setNewPwd] = React.useState('');
-  const [validPwd, setValidPwd] = useState(true); // Initialize with true
+	const [newPwd, setNewPwd] = React.useState('');
+	const [validPwd, setValidPwd] = useState(true); // Initialize with true
 
 
-  const validatePwd = (pwd: string) => {
-    setValidPwd(PWD_REGEX.test(pwd));
-  };
+	const validatePwd = (pwd: string) => {
+		setValidPwd(PWD_REGEX.test(pwd));
+	};
 
-  useEffect(() => {
-    validatePwd(newPwd);
-  }, [newPwd]);
+	useEffect(() => {
+		validatePwd(newPwd);
+	}, [newPwd]);
 
-  const handleSave = () => {
-	if (validPwd) {
-		onSave(newPwd);
-		handleClose();
-	}
-  };
+	const handleSave = () => {
+		if (validPwd) {
+			onSave(newPwd);
+			handleClose();
+		}
+	};
 
-  const handleClose = () => {
-    setNewPwd(''); // Reset the email input value when closing the modal
-    onClose();
-  };
+	const handleClose = () => {
+		setNewPwd(''); // Reset the email input value when closing the modal
+		onClose();
+	};
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNewPwd(event.target.value);
-  };
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setNewPwd(event.target.value);
+	};
 
-  return (
-    <Dialog open={open} onClose={handleClose}>
-      <DialogTitle>Change Password</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          To update your password, please enter the new password below.
-        </DialogContentText>
-        <TextField
-          autoFocus
-          margin="dense"
-          id="password"
-          label="New Password"
-          type="password"
-          fullWidth
-          variant="standard"
-          value={newPwd}
-          onChange={handleChange}
-		  error={!validPwd && newPwd.length > 0}
-          helperText={!validPwd && newPwd.length > 0 && "Invalid password"}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Cancel</Button>
-        <Button onClick={handleSave}>Save</Button>
-      </DialogActions>
-    </Dialog>
-  );
+	return (
+		<Dialog open={open} onClose={handleClose}>
+		<DialogTitle>Change Password</DialogTitle>
+		<DialogContent>
+			<DialogContentText>
+			To update your password, please enter the new password below.
+			</DialogContentText>
+			<TextField
+			autoFocus
+			margin="dense"
+			id="password"
+			label="New Password"
+			type="password"
+			fullWidth
+			variant="standard"
+			value={newPwd}
+			onChange={handleChange}
+			error={!validPwd && newPwd.length > 0}
+			helperText={!validPwd && newPwd.length > 0 && "Invalid password"}
+			/>
+		</DialogContent>
+		<DialogActions>
+			<Button onClick={handleClose}>Cancel</Button>
+			<Button onClick={handleSave}>Save</Button>
+		</DialogActions>
+		</Dialog>
+	);
 };
 
 export default PwdModal;
