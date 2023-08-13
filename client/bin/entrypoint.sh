0b7866dafaca6d08c456d39e85b10ab8b2b05891
Vladimir Nafissi <vlad.nafissi@gmail.com>
2023-08-13 10:52:15 +0200

    ajout de commentaires explicatifs sur le lancement avec docker

--- client/bin/entrypoint.sh
+++ client/bin/entrypoint.sh
@@ -2,4 +2,9 @@
 
 npm install
 
+# After the dependencies are installed, the exec command replaces itself with 
+# the command and arguments provided when executing the script.
+# This allows you to use the script as a wrapper for other commands, 
+# ensuring that the Node.js dependencies are installed before the main command is executed.
+# "$@": This special construct represents all the arguments passed to the script
 exec "$@"
