2ba861c3af3a94fad0cfde6a02c9ec1c8711e525
samoyla <samoyla@hotmail.com>
2023-08-25 16:59:12 +0200

    leaderboard start

--- client/src/components/leaderboard/Leaderboard.tsx
+++ client/src/components/leaderboard/Leaderboard.tsx
@@ -1,18 +1,58 @@
 import Users from '../users/Users'
 import PageWrapper from '../navbar/pageWrapper'
 import Logout from '../logout/Logout'
-import {Box} from '@mui/material'
+import { Box, Table, TableHead, TableBody, TableRow, TableCell, Paper } from '@mui/material';
 
 function Leaderboard() {
+  // Exemple de données pour le leaderboard (à remplacer par vos propres données)
+  const leaderboardData = [
+    { name: 'User 1', gamesPlayed: 10, score: 500 },
+    { name: 'User 2', gamesPlayed: 8, score: 450 },
+    { name: 'User 3', gamesPlayed: 12, score: 600 },
+    
+  ];
+
   return (
-	<PageWrapper>
-		<Box >
-			<h1>Leaderboard</h1>
-			<Users />
-			<Logout />
-		</Box>
-	</PageWrapper>
-  )
+    <PageWrapper>
+      <Box>
+        <h1>Leaderboard</h1>
+        <Table component={Paper}>
+          <TableHead>
+            <TableRow>
+              <TableCell>Name</TableCell>
+              <TableCell>Games Played</TableCell>
+              <TableCell>Score</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {leaderboardData.map((user, index) => (
+              <TableRow key={index}>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.gamesPlayed}</TableCell>
+                <TableCell>{user.score}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <Logout />
+      </Box>
+    </PageWrapper>
+  );
 }
 
-export default Leaderboard
+export default Leaderboard;
+// function Leaderboard() {
+
+
+//   return (
+// 	<PageWrapper>
+// 		<Box >
+// 			<h1>Leaderboard</h1>
+// 			<Users />
+// 			<Logout />
+// 		</Box>
+// 	</PageWrapper>
+//   )
+// }
+
+// export default Leaderboard
