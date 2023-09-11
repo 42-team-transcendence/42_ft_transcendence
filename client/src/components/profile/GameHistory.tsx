57a9b72452be7b5162149fecfd15763a89811cb6
Camille CALAS <ccalas@made-f0Dr5s4.clusters.42paris.fr>
2023-09-11 12:26:27 +0200

    mise en formr other prof

--- client/src/components/profile/GameHistory.tsx
+++ client/src/components/profile/GameHistory.tsx
@@ -26,143 +26,141 @@ const WinTableCell = styled(TableCell)(({ theme }) => ({
 
 function GameHistory() {
 
-const axiosPrivate = useAxiosPrivate();
-const [gameHistory, setGameHistory] = useState([]);
-const [currentUser, setCurrentUser] = useState<any>();
+	const axiosPrivate = useAxiosPrivate();
+	const [gameHistory, setGameHistory] = useState([]);
+	const [currentUser, setCurrentUser] = useState<any>();
 
-const onlineUsers = useOnlineStatus();
-console.log({onlineUsers});
+	const onlineUsers = useOnlineStatus();
+	console.log({onlineUsers});
 
-useEffect(() => { //fetch game data
-	console.log("coucou useEffect")
-	const findAllMyGames = async () =>{
-		try {
-			const response = await axiosPrivate.get('/games/findAllMyGames', {
-				headers: { 'Content-Type': 'application/json'},
-                    withCredentials: true
-			})
-			setGameHistory(response.data);
-			console.log({findAllMyGames:response.data}); 
-		} catch(error: any) {
-			console.log(error.response);
+	useEffect(() => { //fetch game data
+		console.log("coucou useEffect")
+		const findAllMyGames = async () =>{
+			try {
+				const response = await axiosPrivate.get('/games/findAllMyGames', {
+					headers: { 'Content-Type': 'application/json'},
+						withCredentials: true
+				})
+				setGameHistory(response.data);
+				console.log({findAllMyGames:response.data}); 
+			} catch(error: any) {
+				console.log(error.response);
+			}
 		}
-	}
-	findAllMyGames();
-  }, [])
+		findAllMyGames();
+	}, [])
 
-  useEffect(() => { //Fetch current user data
-	const getCurrentUser = async () => { //definition de la fonction
-		try {
-			const response = await axiosPrivate.get('/users/me', {
-				headers: { 'Content-Type': 'application/json'},
-				withCredentials: true
-			})
-			setCurrentUser(response.data);
-		} catch (error:any) {
-			console.log(error.response );
+	useEffect(() => { //Fetch current user data
+		const getCurrentUser = async () => { //definition de la fonction
+			try {
+				const response = await axiosPrivate.get('/users/me', {
+					headers: { 'Content-Type': 'application/json'},
+					withCredentials: true
+				})
+				setCurrentUser(response.data);
+			} catch (error:any) {
+				console.log(error.response );
+			}
 		}
-	}
-	getCurrentUser(); //appel de la fonction
-}, [])
+		getCurrentUser(); //appel de la fonction
+	}, [])
 
-
-  return (
-    <PageWrapper>
-      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Box
-			sx={{
-				display: "flex",
-				backgroundColor: "white",
-				borderRadius: "20px",
-				padding: "5vh",
-				border: "2px solid black",
-			}}
-        >
-          <Box sx={{ width: "100%" }}>
-			<h1 className="typo-game"> Game History</h1>
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }}>
-                <TableHead>
-					<TableRow
-						sx={{
-							"& .MuiTableCell-root": { borderColor: "#FF79AF", borderWidth: 2 },
-							"& .MuiTableRow-root": { borderColor: "#FF79AF", borderWidth: 2 },}}>
-						<TableCell style={{ textAlign: 'center' }}>Name</TableCell>
-						<TableCell style={{ textAlign: 'center' }}>Score</TableCell>
-						<TableCell style={{ textAlign: 'center' }}>Date</TableCell>
-						<TableCell style={{ textAlign: 'center' }}>Result</TableCell>
-					</TableRow>
-                </TableHead>
-                {currentUser && <TableBody> {
-					gameHistory
-					.slice(Math.max(gameHistory.length - 5, 0))
-					.map((game:any, index) => {
-					let adversaire;
-					let my_score;
-					let adv_score;
-					const formattedTimestamp = game.createdAt
-					  ? new Intl.DateTimeFormat("en-GB", {
-						  day: "2-digit",
-						  month: "2-digit",
-						  year: "numeric",
-						  hour: "2-digit",
-						  minute: "2-digit",
-						}).format(new Date(game.createdAt))
-					  : "";
-					  console.log(`current user id = ${currentUser.id}`);
-					if (game.player_1_id === currentUser.id)
-						adversaire = game.player_2;
-					else
-						adversaire = game.player_1;
-					if(adversaire === game.player_2)
-					{
-						my_score = game.player_1_score;
-						adv_score = game.player_2_score;
-					}
-					else
-					{
-						my_score = game.player_2_score;
-						adv_score = game.player_1_score;
-					}
-					return (
-						<TableRow
-							key={index}
-							sx={{
-								"& .MuiTableCell-root": { borderColor: "#FF79AF", borderWidth: 2 },
-								"& .MuiTableRow-root": { borderColor: "#FF79AF", borderWidth: 2 },}} >
-							<TableCell style={{ textAlign: 'center' }}>
-								<Miniature miniatureUser={{
-									nickname: adversaire.nickname,
-									id: adversaire.id,
-									minAvatar: {
-										url: `http://localhost:3333/public/picture/${adversaire.nickname}`,
-										name: adversaire.nickname
+	return (
+		<PageWrapper>
+			<Box sx={{ display: "flex", justifyContent: "space-between" }}>
+				<Box
+					sx={{
+						display: "flex",
+						backgroundColor: "white",
+						borderRadius: "20px",
+						padding: "5vh",
+						border: "2px solid black",
+					}}
+				>
+					<Box sx={{ width: "100%" }}>
+						<h1 className="typo-game"> Game History</h1>
+						<TableContainer component={Paper}>
+							<Table sx={{ minWidth: 650 }}>
+								<TableHead>
+									<TableRow
+										sx={{
+											"& .MuiTableCell-root": { borderColor: "#FF79AF", borderWidth: 2 },
+											"& .MuiTableRow-root": { borderColor: "#FF79AF", borderWidth: 2 },}}>
+										<TableCell style={{ textAlign: 'center' }}>Name</TableCell>
+										<TableCell style={{ textAlign: 'center' }}>Score</TableCell>
+										<TableCell style={{ textAlign: 'center' }}>Date</TableCell>
+										<TableCell style={{ textAlign: 'center' }}>Result</TableCell>
+									</TableRow>
+								</TableHead>
+								{currentUser && <TableBody> {
+									gameHistory
+									.slice(Math.max(gameHistory.length - 5, 0))
+									.map((game:any, index) => {
+									let adversaire;
+									let my_score;
+									let adv_score;
+									const formattedTimestamp = game.createdAt
+									? new Intl.DateTimeFormat("en-GB", {
+										day: "2-digit",
+										month: "2-digit",
+										year: "numeric",
+										hour: "2-digit",
+										minute: "2-digit",
+										}).format(new Date(game.createdAt))
+									: "";
+									console.log(`current user id = ${currentUser.id}`);
+									if (game.player_1_id === currentUser.id)
+										adversaire = game.player_2;
+									else
+										adversaire = game.player_1;
+									if(adversaire === game.player_2)
+									{
+										my_score = game.player_1_score;
+										adv_score = game.player_2_score;
 									}
-								}}></Miniature>
-							</TableCell>
-						<TableCell style={{ textAlign: 'center' }}>{my_score} - {adv_score}</TableCell>
-						<TableCell style={{ textAlign: 'center' }}>{formattedTimestamp}</TableCell>
-						<TableCell style={{ textAlign: 'center' }}>
-									{game.winnerId === 0 ? (
-										"DRAWN GAME"
-									) : game.winnerId === currentUser.id ?(
-										<WinTableCell style={{ textAlign: 'center' }}> WIN</WinTableCell>
-									) : (
-										<LossTableCell style={{ textAlign: 'center' }}>LOSE</LossTableCell>
-									)}
-								
-						</TableCell>
-						</TableRow>
-					)
-				})}
-                </TableBody>}
-              </Table>
-            </TableContainer>
-          </Box>
-        </Box>
-      </Box>
-    </PageWrapper>
-  );
+									else
+									{
+										my_score = game.player_2_score;
+										adv_score = game.player_1_score;
+									}
+									return (
+										<TableRow
+											key={index}
+											sx={{
+												"& .MuiTableCell-root": { borderColor: "#FF79AF", borderWidth: 2 },
+												"& .MuiTableRow-root": { borderColor: "#FF79AF", borderWidth: 2 },}} >
+											<TableCell style={{ textAlign: 'center' }}>
+												<Miniature miniatureUser={{
+													nickname: adversaire.nickname,
+													id: adversaire.id,
+													minAvatar: {
+														url: `http://localhost:3333/public/picture/${adversaire.nickname}`,
+														name: adversaire.nickname
+													}
+												}}></Miniature>
+											</TableCell>
+											<TableCell style={{ textAlign: 'center' }}>{my_score} - {adv_score}</TableCell>
+											<TableCell style={{ textAlign: 'center' }}>{formattedTimestamp}</TableCell>
+											<TableCell style={{ textAlign: 'center' }}>
+												{game.winnerId === 0 ? (
+													"DRAWN GAME"
+												) : game.winnerId === currentUser.id ?(
+													<WinTableCell> WIN</WinTableCell>
+												) : (
+													<LossTableCell>LOSE</LossTableCell>
+												)}
+											</TableCell>
+										</TableRow>
+									)
+								})}
+								</TableBody>}
+							</Table>
+						</TableContainer>
+					</Box>
+				</Box>
+			</Box>
+		</PageWrapper>
+	);
 }
 
 export default GameHistory;
