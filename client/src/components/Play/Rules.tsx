0e45fa5b03d0445f5126daef1c020740e47a0cca
Camille CALAS <ccalas@made-f0Dr5s4.clusters.42paris.fr>
2023-09-11 15:08:56 +0200

    ajout className dans rules

--- client/src/components/Play/Rules.tsx
+++ client/src/components/Play/Rules.tsx
@@ -13,13 +13,13 @@ const Rules:React.FC = () => {
 					<p>Pong, the OG of video games, was born in the wild '70s, when disco balls ruled the dance floor and sideburns were a fashion statement.
 					Created by Atari wizardry, Pong was like virtual tennis but with pixelated paddles, making your grandma's ping pong look so yesterday.<p/>
 
-					<p><span>#1: </span>Two paddles, one on each side, and a bouncing pixel ball. Keep that ball in play, or you'll be the Pong loser of the day!</p>
+					<p><span className="rules-nb">#1: </span>Two paddles, one on each side, and a bouncing pixel ball. Keep that ball in play, or you'll be the Pong loser of the day!</p>
 
-					<p><span>#2: </span>No power-ups or fancy moves here. Just slide your paddle up and down, left and right, and defend with all your might.
+					<p><span className="rules-nb">#2: </span>No power-ups or fancy moves here. Just slide your paddle up and down, left and right, and defend with all your might.
 					Pong parties were all the rage, as friends gathered 'round the TV screen, hooting and hollering, like they'd never seen.
 					Pong's legacy grew, inspiring the gaming revolution we know today, from pixel to VR, it's come a long way!</p>
 
-					<p><span>#3: </span>Score a point when your opponent drops the ball, but don't get too cocky, 'cause Pong can make you fall!</p>
+					<p><span className="rules-nb">#3: </span>Score a point when your opponent drops the ball, but don't get too cocky, 'cause Pong can make you fall!</p>
 
 					<p>History lesson: Pong paved the way, making arcades cool and controllers sway. It's where the gaming journey found its first play.
 					Simplicity is key, with Pong's timeless charm, a game so easy, it'll do no harm.
